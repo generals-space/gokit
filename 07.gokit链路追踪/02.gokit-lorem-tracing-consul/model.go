1553fbdafd7f6aa2432f6401828b0ab5221976b3
general <generals.space@gmail.com>
2019-06-12 22:42:26 +0800

    补充

--- 07.gokit链路追踪/02.gokit-lorem-tracing-consul/model.go
+++ 07.gokit链路追踪/02.gokit-lorem-tracing-consul/model.go
@@ -3,7 +3,6 @@ package lorem_tracing
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 
@@ -46,8 +45,6 @@ func DecodeHealthRequest(_ context.Context, _ *http.Request) (interface{}, error
 
 // EncodeResponse 这是一个通用方法, 将对象转换成json字符串就可以了, 不用在乎对象类型.
 func EncodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
-	fmt.Println("iiiiiiiiiiiiiiiii")
-	fmt.Printf("%+v\n", response)
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	return json.NewEncoder(w).Encode(response)
 }
@@ -62,8 +59,6 @@ func EncodeLoremRequest(_ context.Context, req *http.Request, request interface{
 
 // DecodeLoremResponse 由客户端调用, 将服务端返回的结果格式化成LoremResponse对象
 func DecodeLoremResponse(_ context.Context, resp *http.Response) (interface{}, error) {
-	fmt.Println("ppppppppppppppppp")
-	fmt.Printf("%+v\n", resp.Body)
 	var response LoremResponse
 	err := json.NewDecoder(resp.Body).Decode(&response)
 	if err != nil {
@@ -75,12 +70,9 @@ func DecodeLoremResponse(_ context.Context, resp *http.Response) (interface{}, e
 // DecodeLoremClientRequest 由客户端调用, 将对客户端的请求转换成LoremRequest对象
 // 注意对客户端的请求内容在POST请求体中, 而不是restful路径中.
 func DecodeLoremClientRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	fmt.Println("0000000000000000000")
-	fmt.Printf("%+v\n", r)
 	var request LoremRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		return nil, err
 	}
-	fmt.Println("1111111111111111111")
 	return request, nil
 }
